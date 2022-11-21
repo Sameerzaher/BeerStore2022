2cb63975427e4238b609962ae81ebe4673f18227
Sameer Zaher <sameerzaher@gmail.com>
2022-11-21 13:57:52 +0200

    test

--- beerstore_APP/App.js
+++ beerstore_APP/App.js
@@ -1,11 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 import type {Node} from 'react';
 import { Provider } from 'react-native-paper'
@@ -20,9 +12,7 @@ import {
   Dashboard,
 } from './src/screens'
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
+
   StyleSheet,
   Text,
   useColorScheme,
@@ -37,9 +27,6 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
-
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
 const Section = ({children, title}): Node => {
 const isDarkMode = useColorScheme() === 'dark';
 return (
